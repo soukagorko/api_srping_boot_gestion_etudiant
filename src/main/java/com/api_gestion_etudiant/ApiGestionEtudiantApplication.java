ca507eef382b438f055c05c3abb41810aef033f6
soukagorko <139471801+soukagorko@users.noreply.github.com>
2023-07-13 21:18:58 +0000

    Add files via upload

---
+++ src/main/java/com/api_gestion_etudiant/ApiGestionEtudiantApplication.java
@@ -0,0 +1,13 @@
+package com.api_gestion_etudiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionEtudiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionEtudiantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_gestion_etudiant/ApiGestionEtudiantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_gestion_etudiant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionEtudiantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
